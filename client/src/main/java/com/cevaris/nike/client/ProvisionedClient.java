904a6b7ff567b337497121efca230ddd636b6ee4
Adam Cardenas <cevaris@gmail.com>
2016-07-01 14:57:54 -0600

    got thrift compiling/imported

--- client/src/main/java/com/cevaris/nike/client/ProvisionedClient.java
+++ client/src/main/java/com/cevaris/nike/client/ProvisionedClient.java
@@ -1,4 +1,37 @@
 package com.cevaris.nike.client;
 
-public class ProvisionedClient {
+import com.cevaris.nike.Hello;
+import org.apache.thrift.TException;
+import org.apache.thrift.server.TServer;
+import org.apache.thrift.server.TThreadPoolServer;
+import org.apache.thrift.transport.TServerSocket;
+import org.apache.thrift.transport.TTransportException;
+
+import java.util.Iterator;
+
+public class ProvisionedClient<T> implements Client<T> {
+
+
+    public void write(T payload) {
+        try {
+            TServerSocket serverTransport;
+            serverTransport = new TServerSocket(7911);
+            Hello.Processor processor = new Hello.Processor(new Hello.Iface() {
+                public String hi() throws TException {
+                    return "hellow";
+                }
+            });
+            TServer server = new TThreadPoolServer(
+                    new TThreadPoolServer.Args(serverTransport).processor(processor));
+            System.out.println("Starting server on port 7911 ...");
+            server.serve();
+        } catch (TTransportException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public Iterator<T> subscriber() {
+        return null;
+    }
 }
--- examples/src/main/java/com/cevaris/nike/examples/SimpleClient.java
+++ examples/src/main/java/com/cevaris/nike/examples/SimpleClient.java
@@ -1,7 +1,7 @@
 package com.cevaris.nike.examples;
 
 import com.cevaris.nike.client.Client;
-import com.cevaris.nike.client.InMemoryClient;
+import com.cevaris.nike.client.ProvisionedClient;
 import com.cevaris.nike.util.Data;
 
 import java.util.Iterator;
@@ -9,7 +9,7 @@ import java.util.Iterator;
 public class SimpleClient {
 
     public static void main(String[] args) {
-        Client<Integer> client = new InMemoryClient<Integer>();
+        Client<Integer> client = new ProvisionedClient<Integer>();
 
         Iterator<Integer> intIter = Data.intList(0, 10);
         while (intIter.hasNext()) {
